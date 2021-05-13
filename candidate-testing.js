671bca9e383ab8d1013f10bd592e40dc3513dc35
Samuel Sabin <>
2021-05-13 17:02:07 +0000

    This is a new assignment page and trying to use all the functions so far I can only utilize 2 functions. I did remove the weird 1 that kept popping up.

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,109 +3,70 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName=[];
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+let askName=['Candidate Name: '];
+
 let question;
 let correctAnswer;
-let candidateAnswer;
-let questions;
+let candidateAnswer=[];
+let questions=['1) Who was the first American woman in space? \n','2) True or false: 5000 meters = 5 kilometers. \n', '3) (5 + 3)/2 * 10 = ? \n', '4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? \n', '5) What is the minimum crew size for the ISS? \n'];
 let correctAnswers;
 let candidateAnswers;
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+  for (let i=0; i< askName.length; i++){
+   name = input.question(askName[i]);
+  
+  }
+  return candidateName.push(name);
 
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-
-}
-
-function gradeQuiz(candidateAnswers) {
-
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
-  let grade;
-  
-
-  return grade;
-}
-
-function runProgram() {
-  askForName();
-  // TODO 1.1c: Ask for candidate's name //
-  askQuestion();
-  gradeQuiz(this.candidateAnswers);
-  const input=require('readline-sync');
-
-
-let askName=['Candidate Name: '];
-let userAnswers=[];
-let arrayAnswers=[];  
-let candidatesName=[];
-
-function askForCandidateName(){
-  for (let i=0; i< askName.length; i++){
-   name = input.question(askName[i]);
-  
-  }
-  return candidatesName.push(name);
-}
-
-function askQuestions(){
-  
-  let questions=['1) Who was the first American woman in space? \n','2) True or false: 5000 meters = 5 kilometers. \n', '3) (5 + 3)/2 * 10 = ? \n', '4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2? \n', '5) What is the minimum crew size for the ISS? \n'];
-
-  let arrayAnswers=[];
+  let candidateAnswer=[];
 
   for (let i=0; i< questions.length; i++){
  answers = (input.question(questions[i]));
- arrayAnswers.push((answers));
+ candidateAnswer.push((answers));
 }
-return arrayAnswers;
-
-// userAnswers.push((arrayAnswers));
+return candidateAnswer;
 }
-// user inputs always come in as a String - no need to convert. 
-// Instead of checking specific spellings, see if there's a string method you can use to convert the user input so you only have to check one spelling
-// use a for loop to check the answers in the array of user answers instead of hard coding it *** do this after you get checks for hard coded answers working. ;) 
 
-// function checkAnswers()
-function checkAnswers(){
-let userAnswers = askQuestions(); 
-let checkingAnswers= userAnswers;
+function gradeQuiz(candidateAnswers) {
+  let userAnswers = askQuestion(); 
 
 let initialScore=0;
 let questionCount=0;
 let overallTestStatus='';
-    if (String(checkingAnswers[0]) === 'sally ride' || String(checkingAnswers[0]) === 'Sally Ride' || String(checkingAnswers[0]) ==='sally Ride' || String(checkingAnswers[0]) === 'Sally ride'){
+    if (String(userAnswers[0]) === 'sally ride' || String(userAnswers[0]) === 'Sally Ride' || String(userAnswers[0]) ==='sally Ride' || String(userAnswers[0]) === 'Sally ride'){
       initialScore= 100;
       questionCount= 1;
     }
 
     
-    if (String(checkingAnswers[1]) === 'true' ||String(checkingAnswers[1])==='TRUE'||String(checkingAnswers[1])==='True'){
+    if (String(userAnswers[1]) === 'true' ||String(userAnswers[1])==='TRUE'||String(userAnswers[1])==='True'){
       initialScore = initialScore + 100;
       questionCount = questionCount + 1;
     }
     
-    if(String(checkingAnswers[2])=== '40'){
-      initialScore = initialScore + 100;
-      questionCount = questionCount + 1;
-    }
-
-    
-    if(String(checkingAnswers[3])==='Trajectory'|| String(checkingAnswers[3])==='trajectory'){
+    if(String(userAnswers[2])=== '40'){
       initialScore = initialScore + 100;
       questionCount = questionCount + 1;
     }
 
     
-    if(String(checkingAnswers[4])==='3'){
+    if(String(userAnswers[3])==='Trajectory'|| String(userAnswers[3])==='trajectory'){
+      initialScore = initialScore + 100;
+      questionCount = questionCount + 1;
+    }
+
+    
+    if(String(userAnswers[4])==='3'){
       initialScore= initialScore +100;
       questionCount = questionCount + 1;
     }
@@ -121,54 +82,58 @@ let overallTestStatus='';
       overallTestStatus="failed";
     }
 
-     
-  
-  // console.log(checkingAnswers);
-  // console.log(initialScore);
-  // console.log(totalScore);
-
+  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
 console.log(`
 Candidate Name: ${name}
 1) Who was the first American woman in Space?
-Your Answer: ${checkingAnswers[0]}
+Your Answer: ${userAnswers[0]}
 Correct Answer: Sally Ride\n
 
 2) True or false: 5000 meters = 5 kilometers.
-Your Answer: ${checkingAnswers[1]}
+Your Answer: ${userAnswers[1]}
 Correct Answer: true\n
 
 3) (5+3)/2 * 10 = ?
-Your Answer: ${checkingAnswers[2]}
+Your Answer: ${userAnswers[2]}
 Correct Answer: 40\n
 
 4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?
-Your Answer: ${checkingAnswers[3]}
+Your Answer: ${userAnswers[3]}
 Correct Answer: Trajectory\n
 
 5) What is the minimum crew size for the ISS?
-Your answer: ${checkingAnswers[4]}
+Your answer: ${userAnswers[4]}
 Correct Answer: 3\n
 
 >>> Overall Grade: ${totalScore}% (${finalQuestionCount} of 5 responses correct) <<<
 >>>Status: ${overallTestStatus}<<<`);
 
 
+  // let grade;
+  
+
+  // return grade;
+}
+
+function runProgram() {
+  askForName();
+  // TODO 1.1c: Ask for candidate's name //
+  // askQuestion();
+  // gradeQuiz(this.candidateAnswers);
+  //added this line
+  gradeQuiz();
+  
+
 }
 
 function displayResults(finalResults){
 let candidateName= askForName;
+
 let userAnswers=checkAnswers;
 
-
 }
 
-console.log(askForCandidateName());
-// console.log(askQuestions());
-console.log(checkAnswers()); 
-// displayResults(askForName,askQuestions,checkAnswers);
-// console.log(finalScore);
-}
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
